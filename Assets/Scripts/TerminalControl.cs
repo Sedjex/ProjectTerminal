0d833ab6189fa402c00e1274504f71f328b7965e
Sedjex <kathshem@gmail.com>
2022-06-21 16:35:54 +0300

    5 commit

--- Assets/Scripts/TerminalControl.cs
+++ Assets/Scripts/TerminalControl.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TerminalControl : MonoBehaviour
-{ 
-    enum  Screen { MainMenu, Password, Win };
-    
+{
+    enum Screen { MainMenu, Password, Win };
+
     Screen currentScreen = Screen.MainMenu;
-    
+
     int level;
+    string password;
 
     void Start()
     {
         ShowMainMenu("NoName, ");
     }
-    
+
     void ShowMainMenu(string playerName)
     {
         currentScreen = Screen.MainMenu;
+        level = 0;
         Terminal.ClearScreen();
         Terminal.WriteLine(playerName + "\nякий термінал ви хотіли б взломати?");
         Terminal.WriteLine("\n1 - міська бібліотека");
@@ -26,7 +28,7 @@ public class TerminalControl : MonoBehaviour
         Terminal.WriteLine("\nЗробіть вибір ...");
     }
 
-   
+
     void OnUserInput(string input)
     {
         if (input == "меню")
@@ -37,11 +39,16 @@ public class TerminalControl : MonoBehaviour
         {
             RunMainMenu(input);
         }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
     }
     void GameStart()
     {
         currentScreen = Screen.Password;
         Terminal.WriteLine("Ви обрали " + level + " рівень");
+        Terminal.WriteLine("Введіть пароль: ");
     }
 
     void RunMainMenu(string input)
@@ -53,22 +60,38 @@ public class TerminalControl : MonoBehaviour
         else if (input == "1")
         {
             level = 1;
+            password = "ключ";
             GameStart();
         }
         else if (input == "2")
         {
             level = 2;
+            password = "пістолет";
             GameStart();
         }
         else if (input == "3")
         {
             level = 3;
+            password = "інтерстеллар";
             GameStart();
         }
-        
+
         else
         {
             Terminal.WriteLine("Введіть значення від 1 до 3 або меню");
+        }
+    }
+
+    void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            Terminal.WriteLine("Вітаю! Термінал взламано.");
+        }
+
+        else
+        {
+            Terminal.WriteLine("Не вдалося, спробуйте ще...");
         }
     }
 }
